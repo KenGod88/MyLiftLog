635a6e14c33199728fbf828e35e50218d1a692f3
KenGod88 <ken.godfroid@student.hogent.be>
2024-06-16 22:13:59 +0200

    Added IdentityServer

---
+++ MyLiftLog.IdentityServer/Config.cs
@@ -0,0 +1,32 @@
+﻿using Duende.IdentityServer.Models;
+
+namespace MyLiftLog.IdentityServer
+{
+    public static class Config
+    {
+        public static IEnumerable<IdentityResource> IdentityResources =>
+            new IdentityResource[]
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile()
+            };
+
+        public static IEnumerable<ApiScope> ApiScopes =>
+            new ApiScope[]
+                {
+                new ApiScope(name: "MyLiftLog.Api", displayName: "MyLiftLog API - Full Acces")
+                };
+
+        public static IEnumerable<Client> Clients =>
+            new Client[]
+                {
+                    new Client
+                {
+                    ClientId = "MyLiftLog.Postman.Client",
+                    ClientSecrets = { new Secret("secret".Sha256())},
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    AllowedScopes = { "MyLiftLog.Api" }
+                 }
+                };
+    }
+}
---
+++ MyLiftLog.IdentityServer/Models/ApplicationUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace MyLiftLog.IdentityServer.Models
+{
+    public class ApplicationUser : IdentityUser<Guid>
+    {
+
+    }
+}
