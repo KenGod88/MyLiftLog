61886a945d901fa88e5dcea76f117aa990ae9d02
KenGod88 <ken.godfroid@student.hogent.be>
2024-07-16 21:38:30 +0200

    Added Workout model to UI layer

---
+++ MyLiftLog.UI/Models/Workout.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLiftLog.UI.Models
+{
+    public class Workout
+    {
+       
+        public Guid Id { get; set; }
+
+        [Required]
+        public DateTime Date { get; set; }
+        public string Notes { get; set; }
+        
+    }
+}
