10c7cb6ca4fa59180775fe16117ce351c85d7896
KenGod88 <ken.godfroid@student.hogent.be>
2024-08-27 20:16:47 +0200

    simple layout detailPage + loading of data

--- MyLiftLog.UI/WorkoutDetailPage.xaml.cs
+++ MyLiftLog.UI/WorkoutDetailPage.xaml.cs
@@ -1,13 +1,45 @@
 using MyLiftLog.UI.Models;
+using Newtonsoft.Json;
 
 namespace MyLiftLog.UI;
 
 public partial class WorkoutDetailPage : ContentPage
 {
+
+    public List<Exercise> Exercises { get; set; }
+
     public WorkoutDetailPage(Workout workout)
     {
         InitializeComponent();
+        // Fetch exercises for the workout when the page is initialized
+        LoadWorkoutDetails(workout.Id);
     }
 
-   
+    private async void LoadWorkoutDetails(Guid workoutId)
+    {
+        // Bypass SSL certificate validation for development purposes
+        var handler = new HttpClientHandler();
+        handler.ServerCertificateCustomValidationCallback = (message, cert, chain, errors) => true;
+        var client = new HttpClient(handler);
+
+        var response = await client.GetAsync($"https://localhost:7093/api/workout/{workoutId}");
+        if (response.IsSuccessStatusCode)
+        {
+            var content = await response.Content.ReadAsStringAsync();
+            var workout = JsonConvert.DeserializeObject<Workout>(content);
+
+            // Extract exercises from workout data
+            Exercises = workout.WorkoutExercises.Select(we => we.Exercise).ToList();
+
+            // Bind the data to the CollectionView
+            ExerciseListView.ItemsSource = Exercises;
+        }
+    }
+
+    private void OnBackButtonClicked(object sender, EventArgs e)
+    {
+        Navigation.PopAsync();
+    }
+
+
 }
